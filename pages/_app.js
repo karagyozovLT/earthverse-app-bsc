02c7f99900d5ed03e419ca5bc6cefaa4a8bdf299
karagyozovLT <georgi.karagyozov@looming.tech>
2023-03-04 15:12:28 +0200

    Update components

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import "../styles/globals.css";
 import { WagmiConfig, configureChains, createClient } from "wagmi";
 import { bsc, bscTestnet } from "@wagmi/chains";
 
+import Footer from "../components/Footer";
+import Head from "next/head";
+import Header from "../components/header/Header";
 import { SessionProvider } from "next-auth/react";
 import { publicProvider } from "wagmi/providers/public";
 
@@ -10,14 +13,29 @@ const { provider, webSocketProvider } = configureChains(
   [bscTestnet, bsc],
   [publicProvider()]
 );
-
 const client = createClient({ provider, webSocketProvider, autoConnect: true });
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <WagmiConfig client={client}>
       <SessionProvider session={pageProps.session} refetchInterval={0}>
-        <Component {...pageProps} />
+        <Head>
+          <title>E(AR)TH - NFT EARTH ON BNB CHAIN</title>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta name="description" content="E(AR)TH - NFT EARTH ON BNB CHAIN" />
+        </Head>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <div className="flex-grow">
+            <Component {...pageProps} />
+          </div>
+          <Footer />
+        </div>
       </SessionProvider>
     </WagmiConfig>
   );
