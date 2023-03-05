a126ba09b246578fabf3ea12069be15bd66d7213
karagyozovLT <georgi.karagyozov@looming.tech>
2023-03-05 21:51:09 +0200

    Added new svgs

---
+++ svg/components/VerticalLineIcon.jsx
@@ -0,0 +1,22 @@
+const VerticalLineIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      version="1.1"
+      viewBox="0 0 700 700"
+    >
+      <path
+        fill="none"
+        transform="matrix(5.6 0 0 5.6 70 3.979e-14)"
+        d="m50 5v900"
+        stroke="#5474fc"
+        strokeLinecap="round"
+        strokeMiterlimit="10"
+        strokeWidth="5"
+      />
+    </svg>
+  );
+};
+
+export default VerticalLineIcon;
