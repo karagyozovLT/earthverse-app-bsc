77c13963fb4332a28c9538c7a8a820bda1717ec8
karagyozovLT <georgi.karagyozov@looming.tech>
2023-03-05 21:51:51 +0200

    Added new pages

---
+++ pages/mint-bnfd.jsx
@@ -0,0 +1,9 @@
+const MintBnfd = () => {
+  return (
+    <div className="max-w-7xl mx-auto px-8">
+      <h1 className="text-4xl font-black mt-28">Mint BNFD</h1>
+    </div>
+  );
+};
+
+export default MintBnfd;
